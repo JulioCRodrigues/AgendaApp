8ab70ec90bf4a41bfa848fec8857061ea9340bb8
Julio Rodrigues <julio_cesar.jcr@hotmail.com>
2020-10-21 14:11:53 -0300

    App - Agenda

---
+++ app/src/main/java/julio/com/br/MainActivity.java
@@ -0,0 +1,14 @@
+package julio.com.br;
+
+//imports e pacotes
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
